10e63f3667960c69e96679cd04694de910815193
Michal Skop <michal.skop@kohovolit.eu>
2023-02-15 21:19:40 +0100

    brno govity rebelity sorted

--- brno/scripts/rebeliter.py
+++ brno/scripts/rebeliter.py
@@ -71,6 +71,9 @@ pt['gover'] = round(1000 * gt['govity']) / 10
 pt.rename(columns={'possibly_against_gov': 'possible'}, inplace=True)
 pt['with_gov'] = pt['possible'] - pt['against_gov']
 
+# df.sort_values(by=['group_id'], inplace=True) # sort not working for czech characters
+pt = pt.iloc[pt['group_id'].str.normalize('NFKD').argsort()] # https://stackoverflow.com/a/50217892/1666623
+
 dfr = pt.loc[:, ['voter_id', 'group_way_abs', 'rebeling', 'rebel', 'group_id']]
 dfr.to_csv(localpath + "data/rebelity.v1.csv", index=False)
 
